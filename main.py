a9fde969e479c858e4e7684b0f69e8bd251fde2d
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2018-07-17 14:04:19 -0400

    Fix port conflict with application

--- main.py
+++ main.py
@@ -40,4 +40,4 @@ def auto():
     return json.dumps(get_automatic_evaluations())
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8001, debug=True)
